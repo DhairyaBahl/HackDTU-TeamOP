058661f83676f94dcda5ce5145d74c1f936c2e9d
Rohan Gupta <rohangupta1528@gmail.com>
2021-05-02 18:47:59 +0530

    Revert "GitHub secrets"

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "<your_production_apiKey>",
-    authDomain: "<your_production_authDomain>",
-    databaseURL: "<your_production_databaseURL>",
-    projectId: "<your_production_projectId>",
-    storageBucket: "<your_production_storageBucket>",
-    messagingSenderId: "<your_production_messagingSenderId>",
-    appId: "<your_production_appId>"
+    apiKey: "AIzaSyCa95gHswvTfpsDF395J91HYYcouour_ts",
+    authDomain: "cdn-bpit.firebaseapp.com",
+    databaseURL: "https://cdn-bpit-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "cdn-bpit",
+    storageBucket: "cdn-bpit.appspot.com",
+    messagingSenderId: "273897079717",
+    appId: "1:273897079717:web:9f8ad4a156a5d529979e4c"
 };
 
 const firebaseApp=firebase.initializeApp(firebaseConfig);
